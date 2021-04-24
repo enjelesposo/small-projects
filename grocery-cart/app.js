93c92e65250bb81b972170c7fdefbcbab48196d4
Angelica Esposo <73009213+enjelesposo@users.noreply.github.com>
2021-04-24 14:51:29 +0800

    Add files via upload

--- grocery-cart/app.js
+++ grocery-cart/app.js
@@ -2,54 +2,65 @@ const iceCreamShop = {
     menu: {
         0:{
             name: 'Choco',
-            price: '$3',
+            price: 3,
             imgSrc: './img/choco-ic.jfif'
         },
         1:{
             name: 'Coconut',
-            price: '$3',
+            price: 3,
             imgSrc: './img/coconut-ic.jpg'
         },
         2:{
             name: 'Cookie Dough',
-            price: '$3',
+            price: 5,
             imgSrc: './img/cookie-dough-ic.jpg'
         },
         3:{
             name: 'Cookies N\' Cream',
-            price: '$3',
+            price: 3,
             imgSrc: './img/cookies-n-cream-ic.jpg'
         },
         4:{
             name: 'Mango',
-            price: '$3',
+            price: 3,
             imgSrc: './img/mango-ic.jpg'
         },
         5:{
-            name: 'Match',
-            price: '$3',
+            name: 'Matcha',
+            price: 5,
             imgSrc: './img/matcha-ic.jfif'
         },
         6:{
             name: 'Mint Choco',
-            price: '$3',
+            price: 5,
             imgSrc: './img/mint-choco-ic.jpg'
         },
         7:{
             name: 'Strawberry',
-            price: '$3',
+            price: 3,
             imgSrc: './img/strawberry-ic.jpg'
         },
         8:{
             name: 'Vanilla',
-            price: '$3',
+            price: 3,
             imgSrc: './img/vanilla-ic.jpg'
         }
+    },
+
+    amount: {
+        amountTotal: 0
     },
 
     init(){
         const itemsContainer = document.querySelector('.items__container');
         itemsContainer.appendChild(this._createCards());
+        
+        const addToCartButton = document.querySelectorAll('.item__add-button');
+        addToCartButton.forEach( button => {
+            button.addEventListener('click', (button) => {
+                this.getPrice(button.target.getAttribute('data-price'));
+            });
+        })
         
     },
 
@@ -72,9 +83,12 @@ const iceCreamShop = {
             itemWrapper.appendChild(itemInfoWrapper);
 
             const itemInfo = document.createElement('p');           // creates p for item name and price
-            itemInfo.textContent = `${this.menu[item].name} ${this.menu[item].price}`;
+            itemInfo.textContent = `${this.menu[item].name} $${this.menu[item].price}`;
             const addToCartButton = document.createElement('button');
+            addToCartButton.setAttribute('data-name',this.menu[item].name);
+            addToCartButton.setAttribute('data-price',this.menu[item].price);
             addToCartButton.innerHTML = '<i class="las la-cart-plus"></i>';
+            addToCartButton.classList.add('item__add-button');
             // append itemInfo & addToCartBTn to iteminfoWrapper
             itemInfoWrapper.appendChild(itemInfo);
             itemInfoWrapper.appendChild(addToCartButton);
@@ -83,13 +97,15 @@ const iceCreamShop = {
         return fragments;
     },
 
-    appendIceCream(){
-
+    createCartList(name, price){
+        const listItem = document.createElement('li');
+        listItem.innerText = `${name} Ice Cream ---- $${price}`
+        const removeItemButton = document.createElement('button');
     },
 
-    getTotal(){
-
-    },
+    getPrice(price){
+        this.amount.amountTotal = this.amount.amountTotal + parseFloat(price);
+    }
 }
 
 window.addEventListener('DOMContentLoaded', function(){
